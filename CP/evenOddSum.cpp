d79dcfca7c051e99ce86149c0c14eb526d50d25b
Sunder Kumar <sunderkumar2506@gmail.com>
2022-05-16 22:47:12 +0530

    CP Day-1 16 May 2022

---
+++ CP/evenOddSum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    // int* arr = new int[n];
+    int data;
+    int oddSum = 0, evenSum = 0;
+    for(int i = 0; i < n; i++){
+        cin>>data;
+        if(i % 2 == 0 && data % 2 == 0){
+            evenSum += data;
+        }
+        else if(i % 2 != 0 && data % 2 != 0){
+            oddSum += data;
+        }
+    }
+    cout<<evenSum<<" "<<oddSum<<endl;
+}
