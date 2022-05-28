20e22870d36af0b8eac43eeb70c7db1a718810d2
Sunder Kumar <sunderkumar2506@gmail.com>
2022-05-28 13:34:20 +0530

    CP Folder First Commit

---
+++ CP/GeeksAlgorithm/Searching and Sorting/binarySearch.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int midIdx(int si, int ei){
+
+}
+
+int main(){
+    std::vector<int> arr;
+    int data;
+    std::cin>>data;
+
+    while(data != -1){
+        arr.push_back(data);
+        std::cin >> data;
+    }
+
+}
